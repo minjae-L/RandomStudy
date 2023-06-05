ef5d42ad32b77b0831af2fa897bdfd51d327df1e
leeminjae <qlqlzbto@naver.com>
2023-06-05 11:42:13 +0900

    Initial Commit

---
+++ RandomStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomStudy
+//
+//  Created by 이민재 on 2023/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
