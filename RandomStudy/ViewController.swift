a681412567129f4e5f45e0ea452e7dc19cd9951c
leeminjae <qlqlzbto@naver.com>
2023-06-05 22:21:46 +0900

    앱의 구조 만들기

--- RandomStudy/ViewController.swift
+++ RandomStudy/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let test = UILabel()
+    let button = UIButton()
+    let toolbar = UIToolbar()
+    let toolbarItem1 = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: nil)
+    var items: [UIBarButtonItem] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(test)
+        view.addSubview(toolbar)
+        
+        test.text = "text"
+        test.translatesAutoresizingMaskIntoConstraints = false
+        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        self.navigationItem.title = "Main"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(presentAddVC))
+        items.append(toolbarItem1)
+        
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        toolbar.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 0).isActive = true
+        toolbar.bottomAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: 0).isActive = true
+        toolbar.trailingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.trailingAnchor, multiplier: 0).isActive = true
+        toolbar.setItems([toolbarItem1], animated: true)
+        
+        button.setTitle("Show", for: .normal)
+        view.addSubview(button)
+        button.backgroundColor = .systemBlue
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
 
+    @objc private func didTapButton() {
+
+    }
+    
+    @objc private func presentAddVC() {
+        let vc = AddCategoryViewController()
+        
+        let navVC = UINavigationController(rootViewController: vc)
+        navVC.modalPresentationStyle = .fullScreen
+        
+        present(navVC, animated: true)
+    }
 
 }
-
