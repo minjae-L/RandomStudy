bb915fa69d4bf324b394cbcb046c5daa704a90d0
이민재 <qlqlzbto@gmail.com>
2024-04-23 23:28:01 +0900

    Revert "no message"
    This reverts commit 488365003d5d8439bc6ba337eb18ff67ee9b7d56.

--- RandomStudy/Controllers/AddPopUpViewController.swift
+++ RandomStudy/Controllers/AddPopUpViewController.swift
@@ -38,9 +38,7 @@ class AddPopUpViewController: UIViewController {
         stv.translatesAutoresizingMaskIntoConstraints = false
         return stv
     }()
-    
-//    private var titleLabel: UILabel = {
-    private lazy var titleLabel: UILabel = {
+    private var titleLabel: UILabel = {
         let lb = UILabel()
         lb.font = .boldSystemFont(ofSize: 20)
         lb.text = "추가하기"
@@ -143,21 +141,6 @@ class AddPopUpViewController: UIViewController {
             delegate?.sendedDataFromPopUp()
             self.dismiss(animated: true)
         }
-        if text == "" {
-            errorLabel.text = "공백은 추가할 수 없습니다."
-            errorLabel.isHidden = false
-        } else {
-            if viewModel.isContainsElement(str: text) {
-                errorLabel.text = "같은 목록이 존재합니다."
-                errorLabel.isHidden = false
-            } else {
-                errorLabel.isHidden = true
-                viewModel.addData(str: text)
-                delegate?.sendedDataFromPopUp()
-                self.dismiss(animated: true)
-            }
-        }
-        print("delegate: \(delegate)")
     }
     @objc func checkBlankTextField(_ sender: UITextField) {
         if sender.text?.count == 0 {
