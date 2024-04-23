4405412fb2de67d133eaff9619be9a883ea4f656
leeminjae <54211648+minjae-L@users.noreply.github.com>
2024-04-23 21:01:46 +0900

    Revert "Revert "Feature/add popup view""

--- RandomStudy/Controllers/AddViewController.swift
+++ RandomStudy/Controllers/AddViewController.swift
@@ -13,12 +13,10 @@ class AddViewController: UIViewController {
     // 뷰모델 선언
     private var tableView = UITableView()
     private var viewModel = AddViewModel()
-    
     private func addSubView() {
         view.addSubview(tableView)
         settingUI()
     }
-    
     // 데이터 바인딩
     private func bindings() {
         viewModel.delegate = self
@@ -49,38 +47,29 @@ class AddViewController: UIViewController {
         super.viewDidLoad()
         addSubView()
         bindings()
+        
     }
-    
     
     // 추가하기 버튼 이벤트
     @objc func addCategory() {
-        let alert = UIAlertController(title: "추가하기", message: "", preferredStyle: .alert)
-        let addAction = UIAlertAction(title: "추가", style: .default) { (addAction) in
-            guard let text = alert.textFields?[0].text else { return }
-            if !self.viewModel.isContainsElement(str: text) {
-                self.viewModel.addData(str: text)
-            } else {
-                let errorAlert = UIAlertController(title: "오류", message: "같은 목록이 이미 있습니다.", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "확인", style: .default)
-                errorAlert.addAction(okAction)
-                self.present(errorAlert, animated: true)
-            }
-        }
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
-        
-        addAction.isEnabled = false
-        alert.addAction(addAction)
-        alert.addAction(cancelAction)
-        alert.addTextField() { (textField) in
-            textField.becomeFirstResponder()
-            textField.returnKeyType = .done
-            textField.addTarget(alert, action: #selector(alert.checkTextFieldBlank(_:)), for: UIControl.Event.editingChanged)
-        }
-        self.present(alert, animated: true, completion: nil)
+        let addPopUpView = AddPopUpViewController()
+        addPopUpView.delegate = self
+        addPopUpView.modalPresentationStyle = .overFullScreen
+        addPopUpView.modalTransitionStyle = .crossDissolve
+        self.present(addPopUpView, animated: true, completion: nil)
     }
     
 }
-// cell Button Action
+// MARK: Delegates
+extension AddViewController: AddPopUpViewControllerDelegate {
+    func sendedDataFromPopUp() {
+        viewModel.fetchData()
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+}
+
 extension AddViewController: AddViewControllerButtonDelegate {
     func cellDeleteButtonTapped(name: String) {
         viewModel.removeData(name: name)
@@ -88,7 +77,6 @@ extension AddViewController: AddViewControllerButtonDelegate {
 }
 extension AddViewController: AddViewModelDelegate {
     func didUpdate(with value: [StudyModel]) {
-        print("add delegate didupdate")
         DispatchQueue.main.async { [weak self] in
             self?.tableView.reloadData()
         }
@@ -126,14 +114,5 @@ extension AddViewController: UITableViewDataSource, UITableViewDelegate {
     }
 }
 
-// 텍스트필드 공백 검사
-extension UIAlertController {
-    @objc func checkTextFieldBlank(_ sender: UITextField) {
-        if (sender.text?.count == 0) {
-            self.actions[0].isEnabled = false
-        } else {
-            self.actions[0].isEnabled = true
-        }
-    }
-}
+
 
