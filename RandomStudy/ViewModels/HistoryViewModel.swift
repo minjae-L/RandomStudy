3f1fd21c148ac95dba20ad603274b561fca9bc5d
이민재 <qlqlzbto@gmail.com>
2024-05-23 11:00:45 +0900

    HistoryVC firebase로부터 데이터 받고 UI그리기

--- RandomStudy/ViewModels/HistoryViewModel.swift
+++ RandomStudy/ViewModels/HistoryViewModel.swift
@@ -7,12 +7,21 @@
 
 import Foundation
 
+protocol HistoryViewModelDelegate: AnyObject {
+    func fetchedData()
+}
 final class HistoryViewModel {
     
-    var completions: [StudyModel] = DBHelper.shared.readData(tableName: "history", column: ["name", "done", "date"])
-    private let tableName = "history"
-    private let column = ["name", "done", "date"]
-    
+    var completions: [StudyModel] = [] {
+        didSet {
+            delegate?.fetchedData()
+        }
+    }
+    weak var delegate: HistoryViewModelDelegate?
+    init () {
+        print("HistoryVM:: init ")
+        self.fetchData()
+    }
     var count: Int {
         return completions.count
     }
@@ -22,5 +31,13 @@ final class HistoryViewModel {
     
     var dateArray: [String] {
         return Array(Set(completions.compactMap { $0.date })).sorted()
+    }
+    private func fetchData() {
+        DBHelper.shared.getDataFromFirebase(dataName: "history") { [weak self] dataModel in
+            guard let self = self,
+                  let data = dataModel
+            else { return }
+            self.completions = data
+        }
     }
 }
