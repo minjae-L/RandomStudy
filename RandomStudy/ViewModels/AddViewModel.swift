d155a93e94f9c954038c42bbc12f52d63bf0dff0
이민재 <qlqlzbto@naver.com>
2024-09-04 00:22:40 +0900

    뷰모델에서 직접 Firebase에 접근하는 방식에서 FireabseManager에 선언된 메서드를 호출하는 방식으로 변경

--- RandomStudy/ViewModels/AddViewModel.swift
+++ RandomStudy/ViewModels/AddViewModel.swift
@@ -44,42 +44,20 @@ final class AddViewModel {
     }
     
     // 배열에 값 추가
-    func addData(str: String) {
+    func insert(str: String) {
         if str == "" { return }
-        guard let uid = Auth.auth().currentUser?.uid else { return }
-        let data: [[String: String]] = [["name": str]]
-        do {
-            try db.collection("users").document(uid).updateData(["data": FieldValue.arrayUnion(data)])
-            print("addVM:: Success Data Write")
-        } catch {
-            print("addVM:: Fail Data Write")
-        }
+        let data = FirebaseDataModel(name: str)
+        FirebaseManager.shared.setDataToFirebase(data: data)
         self.fetchData()
     }
-    
-    func removeData(name: String) {
-        guard let uid = Auth.auth().currentUser?.uid else { return }
-        let removed: [[String: String]] = [["name": name]]
-        print("AddVM:: removed: \(removed)")
-        do {
-            try db.collection("users").document(uid).updateData(["data": FieldValue.arrayRemove(removed)])
-            print("addVM:: Success Data Removed")
-        } catch {
-            print("addVM:: Fail Data Removed")
-        }
+    func remove(name: String) {
+        let data = FirebaseDataModel(name: name)
+        FirebaseManager.shared.removeDataFromFirebase(data: data)
         self.fetchData()
     }
     func fetchData() {
-        FirebaseManager.shared.getDataFromFirebase(dataName: "data") { [weak self] dataModel in
-            guard let self = self, let data = dataModel else {
-                self?.elements = []
-                return
-            }
-            self.elements = data.filter{$0.date == nil && $0.done == nil}
-        }
+        let arr = FirebaseManager.shared.elements.filter{ $0.date == nil && $0.done == nil}
+        self.elements = arr
     }
 }
-
-
-
 
