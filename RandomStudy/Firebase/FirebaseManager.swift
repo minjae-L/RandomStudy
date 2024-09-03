1ce7b8c72a710f7539bd830f737a52307d9c8c16
이민재 <qlqlzbto@naver.com>
2024-09-04 00:10:52 +0900

    데이터 마이그레이션 메서드를 익스텐션으로 이동

--- RandomStudy/Firebase/FirebaseManager.swift
+++ RandomStudy/Firebase/FirebaseManager.swift
@@ -31,50 +31,8 @@ class FirebaseManager {
             completion(user?.uid)
         }
     }
-    // 데이터 마이그레이션
-    func dataMigration() {
-        print("Firebase:: dataMigration:: Excuted")
-        self.getUserInfo { [weak self] uid in
-            guard let self = self,
-                  let uid = uid
-            else { return }
-            do {
-                try Firestore.firestore().collection("users").document(uid).updateData(["data": FieldValue.arrayUnion(getDataFromSQLite())])
-                print("Firebase:: dataMigration:: success written document")
-            } catch {
-                print("Firebase:: dataMigration:: fail writing document")
-            }
-            DBHelper.shared.resetAllTable()
-        }
-    }
-    // 마이그레이션 전에 SQLite로부터 데이터 불러오기
-    func getDataFromSQLite() -> [[String: Any]]{
-        var dataArray = [[String: Any]]()
-        var data = DBHelper.shared.readData(tableName: "todo", column: column)
-        
-        if !data.isEmpty {
-            for j in data {
-                dataArray.append(["name": j.name!])
-            }
-        }
-        
-        data = DBHelper.shared.readData(tableName: "study", column: column)
-        if !data.isEmpty {
-            for j in data {
-                dataArray.append(["name": j.name!, "done": (j.done == "0" ? false : true)])
-            }
-        }
-        
-        data = DBHelper.shared.readData(tableName: "history", column: column)
-        if !data.isEmpty {
-            for j in data {
-                dataArray.append(["name": j.name!, "done": true, "date": j.date!])
-            }
-        }
-        return dataArray
-    }
     // Firebase로부터 데이터 불러오기
-    func getDataFromFirebase(dataName: String, completion: @escaping ([FirebaseDataModel]?) -> ()) {
+    func getDataFromFirebase(completion: @escaping ([FirebaseDataModel]?) -> ()) {
         guard let uid = Auth.auth().currentUser?.uid else {
             completion(nil)
             return
@@ -118,5 +76,39 @@ class FirebaseManager {
         } catch {
             print("DBHelper:: do delete fail")
         }
+
+// MARK: Data Migration
+extension FirebaseManager {
+    // 데이터 마이그레이션
+    func dataMigration() {
+        print("Firebase:: dataMigration:: Excuted")
+        Firestore.firestore().collection("users").document(uid).updateData(["data": FieldValue.arrayUnion(getDataFromSQLite())])
+        DBHelper.shared.resetAllTable()
+    }
+    // 마이그레이션 전에 SQLite로부터 데이터 불러오기
+    func getDataFromSQLite() -> [[String: Any]]{
+        var dataArray = [[String: Any]]()
+        var data = DBHelper.shared.readData(tableName: "todo", column: column)
+        
+        if !data.isEmpty {
+            for j in data {
+                dataArray.append(["name": j.name!])
+            }
+        }
+        
+        data = DBHelper.shared.readData(tableName: "study", column: column)
+        if !data.isEmpty {
+            for j in data {
+                dataArray.append(["name": j.name!, "done": (j.done == "0" ? false : true)])
+            }
+        }
+        
+        data = DBHelper.shared.readData(tableName: "history", column: column)
+        if !data.isEmpty {
+            for j in data {
+                dataArray.append(["name": j.name!, "done": true, "date": j.date!])
+            }
+        }
+        return dataArray
     }
 }
