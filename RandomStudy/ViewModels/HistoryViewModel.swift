e8cfda670ae39564878af1a2a8898f1b5fc3f584
이민재 <qlqlzbto@naver.com>
2024-11-01 23:26:06 +0900

    UI에 그려질 데이터배열 분리 / 검색어를 저장하고 변경마다 필터링된 데이터배열로 최신화 -> UI 다시그리기

--- RandomStudy/ViewModels/HistoryViewModel.swift
+++ RandomStudy/ViewModels/HistoryViewModel.swift
@@ -11,10 +11,31 @@ protocol HistoryViewModelDelegate: AnyObject {
     func fetchedData()
 }
 final class HistoryViewModel {
-    
-    private(set) var completions: [FirebaseDataModel] = [] {
+    // Firebase로부터 받은 "기록" 데이터
+    private(set) var historyData: [FirebaseDataModel] = [] {
         didSet {
+            makeCompletions()
+            getSectionCellCountArray()
+        }
+    }
+    // Cell에 뿌려질 기록 데이터 배열
+    var completions: [FirebaseDataModel] = [] {
+        didSet {
+            print("DIDSET completions(\(completions.count))")
+            print(completions)
             delegate?.fetchedData()
+        }
+    }
+    // Section별 Cell의 개수를 저장한 배열 index: section
+    var sectionCellCount = [Int]()
+    // 검색창에 검색어 쓰여져있는중인지 판단하는 변수
+    var searchEditing: Bool = false
+    // 검색창의 검색어
+    // 검색어가 변경될때 마다 Cell내용 변경
+    var searchText: String = "" {
+        didSet {
+            makeCompletions()
+            getSectionCellCountArray()
         }
     }
     weak var delegate: HistoryViewModelDelegate?
@@ -24,13 +45,34 @@ final class HistoryViewModel {
     }
     var count: Int {
         return completions.count
+    // Firebase에서 데이터 가져오기
+    private func fetchData() {
+        historyData = FirebaseManager.shared.getFilteredData(type: .history)
     }
     var dateCount: Int {
         return Array(Set(completions.compactMap { $0.date })).count
+    // Cell에 뿌려질 데이터배열 최신화 메서드
+    private func makeCompletions(){
+        var output: [FirebaseDataModel] = []
+        if searchEditing {
+            output = filteredCompletions(text: searchText)
+        } else {
+            output = historyData
+        }
+        completions = output
     }
     
     var dateArray: [String] {
         return Array(Set(completions.compactMap { $0.date })).sorted()
+    // Cell에 뿌려지는 데이터배열을 검색어와 일치하는 배열로 변환하는 메서드
+    private func filteredCompletions(text: String) -> [FirebaseDataModel] {
+        var output: [FirebaseDataModel] = []
+        for element in historyData {
+            if isContains(searchText: text, data: element) {
+                output.append(element)
+            }
+        }
+        return output
     }
     private func fetchData() {
         completions = FirebaseManager.shared.getFilteredData(type: .history)
